c95d0890e22b09595ae93c81662fd76af577854b
meganhall719 <megan.hall@utexas.edu>
2025-05-02 15:44:07 -0500

    analyzr

---
+++ Creating Package.R
@@ -0,0 +1,15 @@
+#loading necessary packages
+library(devtools)
+library(roxygen2)
+library(withr)
+library(tidyverse)
+library(usethis)
+
+has_devel()
+#spit back you rsystem is ready to build packages!
+usethis::create_package("~/Development/Repos/AnalyzeR")
+
+install.packages("~/Users/meganhall/Development/Repos/AnalyzeR_0.0.0.9000.tar.gz", repos = NULL, type = "source",
+                 lib = "/Library/Frameworks/R.framework/Versions/3.5/Resources/library")
+install.packages("/Users/meganhall/Development/Repos/AnalyzeR_0.0.0.9000.tar.gz", repos = NULL, type = "source")
+library(AnalyzeR)
